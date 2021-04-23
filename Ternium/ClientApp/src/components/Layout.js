382b33cdf68a3a6e31ecb3be77a7378eb6e327d0
seawar741 <juanca741@gmail.com>
2021-04-23 16:24:56 -0500

    Agregar archivos de proyecto.

---
+++ Ternium/ClientApp/src/components/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { NavMenu } from './NavMenu';
+
+export class Layout extends Component {
+  static displayName = Layout.name;
+
+  render () {
+    return (
+      <div>
+          {this.props.children}
+      </div>
+    );
+  }
+}
