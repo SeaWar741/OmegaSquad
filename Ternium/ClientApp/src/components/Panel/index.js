75d58894414987d1ef46073d1a1c5f49c5501dc0
seawar741 <juanca741@gmail.com>
2021-04-27 20:06:26 -0500

    added minor fixes

--- Ternium/ClientApp/src/components/Panel/index.js
+++ Ternium/ClientApp/src/components/Panel/index.js
@@ -1,74 +1,159 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
+const drawerWidth = 200;
 
-const useStyles = makeStyles((theme) =>({
-    sidebar:{
-        margin: "0",
-        padding: "0",
-        width: "200px",
-        backgroundColor: "#f1f1f1",
-        position: "fixed",
-        height: "100%",
-        overflow: "auto",
-        '@media screen and (max-width: 800px)' : {
-            width: "100%",
-            height: "auto",
-            position: "relative"
-        }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
     },
-    elementMenu:{
-        display: "block",
-        color: "black",
-        padding: "16px",
-        textDecoration: "none",
-        '&:active': {
-            backgroundColor: "#4CAF50",
-            color: "white"
-        },
-        '&:hover:not': {
-            backgroundColor: "#555",
-            color: "white"
-        },
-        '@media screen and (max-width: 800px)' : {
-            float: "left"
-        },
-        '@media screen and (max-width: 400px)' : {
-            textAlign: "center",
-            float: "none"
-        }
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
     },
-    content:{
-        marginLeft: "200px",
-        padding: "1px 16px",
-        height: "1000px",
-        '@media screen and (max-width: 800px)' : {
-            marginLeft: "0"
-        }
-    }
-}))
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: "5rem"
+  },
+}));
 
-const Panel = ({classes}) =>{
-    classes = useStyles();
+function Panel(props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-    return (
-        <div>
-            <div className={classes.sidebar}>
-                <a className={classes.elementMenu}  href="/home">Home</a>
-                <a className={classes.elementMenu}  href="/game">Game</a>
-                <a className={classes.elementMenu}  href="/leaderboard">Leaderboard</a>
-                <a className={classes.elementMenu}  href="/stats">Stats</a>
-            </div>
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+        <List>
+            <ListItem button key={"Hola"}>
+                <ListItemIcon> <InboxIcon /> </ListItemIcon>
+            </ListItem>
+            <ListItem button key={"Hola"}>
+                <ListItemIcon> <InboxIcon /> </ListItemIcon>
+            </ListItem>
+            <ListItem button key={"Hola"}>
+                <ListItemIcon> <InboxIcon /> </ListItemIcon>
+            </ListItem>
+            <ListItem button key={"Hola"}>
+                <ListItemIcon> <InboxIcon /> </ListItemIcon>
+            </ListItem>
+        </List>
+    </div>
+  );
 
-            <div className={classes.content}>
-                <h2>Responsive Sidebar Example</h2>
-                <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
-                <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
-                <h3>Resize the browser window to see the effect.</h3>
-            </div>
-        </div>
-    );
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
+  );
 }
+
+Panel.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
 
 export default Panel;
